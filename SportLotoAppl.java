20b8e402a482f89fa5bb7cb9749f0c752b0001b5
AleksandrRiabinin <alexr2141@gmail.com>
2022-11-04 15:38:45 +0200

    homework 03.11.22 rev.1

--- SportLotoAppl.java
+++ SportLotoAppl.java
@@ -14,23 +14,25 @@ public class SportLotoAppl {
 		int e = 0;
 		int f = 0;
 		
-		do a = (getRandomInt(1, 6)); 
+		a = (getRandomInt(1, 6)); 
 		// took the value 6 instead of 49, in order to be sure that Loto works
-			while (a == b || a == c || a == d || a == e || a == f);
 		do b = (getRandomInt(1, 6)); 
-			while (b == a || b == c || b == d || b == e || b == f);
+			while (b == a);
 		do c = (getRandomInt(1, 6)); 
-			while (c == a || c == b || c == d || c == e || c == f);
-		 do d = (getRandomInt(1, 6)); 
-		 	while (d == a || d == b || d == c || d == e || d == f);
-		 do e = (getRandomInt(1, 6)); 
-		 	while (e == a || e == b || e == c || e == d || e == f);
-		 do f = (getRandomInt(1, 6)); 
+			while (c == a || c == b);
+		do d = (getRandomInt(1, 6)); 
+		 	while (d == a || d == b || d == c);
+		do e = (getRandomInt(1, 6)); 
+		 	while (e == a || e == b || e == c || e == d);
+		do f = (getRandomInt(1, 6)); 
 		 	while (f == a || f == b || f == c || f == d || f == e); 
 			
 		System.out.print(a + " " + b + " " + c + " " + d + " " + e + " " + f);
-		
+//		for (int i = 0; i < 6; i++) {
+//			System.out.print(getRandomInt(1, 49) + " ");
+//		}
 		}
+	
 	
 	public static int getRandomInt(int min,int max) {
 		return (int) (min + Math.random() * (max - min +1));
